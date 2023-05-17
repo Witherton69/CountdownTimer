bcf83d92826914fbf5c88f5a643b7abcce14d448
Witherton <katelyn.gosnell@stu.barren.kyschools.us>
2023-05-17 08:45:38 -0500

    Initial Commit

---
+++ CountdownTimer/CountdownTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownTimerApp.swift
+//  CountdownTimer
+//
+//  Created by Katelyn Gosnell on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CountdownTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
